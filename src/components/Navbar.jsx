fde4fca7456b22552207d2a0afa43df45f48784f
mohasan <hasannejadi80@gmail.com>
2024-06-15 10:36:23 +0330

    Add logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 function Navbar() {
+  const { logout } = useContext(AuthContext);
+
   return (
     <div>
-      <nav className="flex justify-between bg-gray-800 py-6 px-6">
+      <nav className="flex justify-between items-center bg-gray-800 py-6 px-6">
         <h2 className="text-xl font-bold text-blue-400">Docker Registry</h2>
-        <ul className="flex gap-10">
+        <ul className="flex gap-10 items-center">
           <li>
             <Link to="/" className="text-white font-bold text-lg">
               Home
@@ -21,6 +24,9 @@ function Navbar() {
             <Link to="/add-user" className="text-white font-bold text-lg">
               Add User
             </Link>
+          </li>
+          <li>
+            <button onClick={logout} className="btn-primary bg-red-500 font-bold px-4 py-1 focus:outline-none">Logout</button>
           </li>
         </ul>
       </nav>
